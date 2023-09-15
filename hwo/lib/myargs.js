9f07ad0593b1cdc8d45430b1880760adc5a1466a
harry.wong <hwong23@gmail.com>
2023-09-15 12:39:13 -0500

    upd

--- hwo/lib/myargs.js
+++ hwo/lib/myargs.js
@@ -1,14 +1,9 @@
 // HWO
 var argv = Java.type("org.eclipse.core.runtime.Platform").getApplicationArgs()
 
-// for (let j = 0; j < argv.length; j++) {
-//     console.log(j + ' -> ' + (argv[j]));
-// }
-
 function getArgumento (miArg) {
     // índice myArg
     const indice = argv.indexOf('-'+miArg);
-    // console.log('myArg: ' + indice + ' -> ' + argv[indice+1]);
-    if (indice != -1) { return argv[indice+1]};
+    if (indice != -1) { return argv[indice+1] };
 }
 

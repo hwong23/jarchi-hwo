35a60d2b2a49c09968f3fdfcc8e962222e552c6f
harry.wong <hwong23@gmail.com>
2023-10-17 12:41:31 -0500

    crrcn

--- hwo/sh/archiMdHtml.sh
+++ hwo/sh/archiMdHtml.sh
@@ -5,8 +5,8 @@
 # $3: origen HTML
 
 # MD
-cp $1/*.md $2/content/
-cp $1/images/*.png $2/content/images/
+mv $1/*.md $2/content/
+mv $1/images/*.png $2/content/images/
 
 # HTML
 cp $3/*.html $2/webpage/

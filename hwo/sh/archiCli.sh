c916d88135ce35ad0c1f3b9302375f7c0826d334
hwo <hwong23@gmail.com>
2023-10-16 20:55:44 -0500

    sh2

--- hwo/sh/archiCli.sh
+++ hwo/sh/archiCli.sh
@@ -1,9 +1,12 @@
-# $1: ruta usuario
+# $1: ruta usuario repositorio git
 # $2: ruta y modelo arq
-# $3: ruta programas ajs
+# $3: ruta programa ajs
 
-# exportSingle-md
-/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $1/$2 --script.runScript $1/jarchi-hwo/hwo/exportSingle-md.ajs -vistaDocumental $4
+
+/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $1/$2 --script.runScript $1/jarchi-hwo/hwo/$3 -vistaDocumental $4
+
+status=$?
 
 # exportSingle-htmlCLI
-/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $1/$2 --script.runScript $1/jarchi-hwo/hwo/exportSingle-htmlCLI.ajs -vistaDocumental $4
+[ $status -eq 0 ] && /Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $1/$2 --script.runScript $1/jarchi-hwo/hwo/exportSingle-htmlCLI.ajs -vistaDocumental $4 || echo "ERR"
+
--- hwo/sh/archiMdHtml.sh
+++ hwo/sh/archiMdHtml.sh
@@ -1,5 +1,9 @@
 # Actualiza documentos MD generados por Archi
 
+# $1: origen MD
+# $2: destino
+# $3: origen HTML
+
 # MD
 mv $1/*.md $2/content/
 mv $1/images/*.png $2/content/images/

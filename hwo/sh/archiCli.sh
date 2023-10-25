4f10eb63a24cad6383504c109ef400d9dbc7f4f2
harry.wong <hwong23@gmail.com>
2023-10-25 13:15:32 -0500

    archicli

--- hwo/sh/archiCli.sh
+++ hwo/sh/archiCli.sh
@@ -3,7 +3,7 @@
 # $3: ruta programa ajs
 
 
-# /Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $1/$2 --script.runScript $1/jarchi-hwo/hwo/$3 -vistaDocumental $4
+/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $1/$2 --script.runScript $1/jarchi-hwo/hwo/$3 -vistaDocumental $4
 
 status=$?
 # exportSingle-htmlCLI

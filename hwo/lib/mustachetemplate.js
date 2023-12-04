98cbe146ae447b8541951017cb0a058b0893a689
harry.wong <hwong23@gmail.com>
2023-12-04 17:04:14 -0500

    cli-tmplt-ok

--- hwo/lib/mustachetemplate.js
+++ hwo/lib/mustachetemplate.js
@@ -2,15 +2,6 @@ load(__DIR__ + 'nashorn-polyfills.js');
 load(__DIR__ + 'underscore-min.js');
 load(__DIR__ + 'marked.min.js');
 
-// funciones con argumento plantillas base 
-var tplDocumentation;
-var tplMainReport = _.template(readFully(__DIR__ + '../templates/newmain-report-md.tpl', 'UTF-8'));
-var tplViewTitle = _.template(readFully(__DIR__ + '../templates/view-title-md.tpl', 'UTF-8'));
-var tplViewDiagram = _.template(readFully(__DIR__ + '../templates/view-diagram-md.tpl', 'UTF-8'));
-var tplElement = function() {};
-var tplElementFila = function() {};
-var tplRelationship = _.template(readFully(__DIR__ + '../templates/relationship.tpl', 'UTF-8'));
-
 
 // Use Mustache.js-style templating
 _.templateSettings = {

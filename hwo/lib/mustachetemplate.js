7a2067a0a70ee1c13416429b62aeedf25e6e0c73
harry.wong <hwong23@gmail.com>
2023-12-03 21:44:44 -0500

    cli-tmplt-7

--- hwo/lib/mustachetemplate.js
+++ hwo/lib/mustachetemplate.js
@@ -15,6 +15,15 @@ var tplDocumentation = _.template(readFully(__DIR__ + '../templates/documentatio
 var tplElement = _.template(readFully(__DIR__ + '../templates/element.tpl', 'UTF-8'));
 var tplRelationship = _.template(readFully(__DIR__ + '../templates/relationship.tpl', 'UTF-8'));
 
+var tplDocumentation;
+var tplMainReport = _.template(readFully(__DIR__ + '../templates/newmain-report-md.tpl', 'UTF-8'));
+var tplViewTitle = _.template(readFully(__DIR__ + '../templates/view-title-md.tpl', 'UTF-8'));
+var tplViewDiagram = _.template(readFully(__DIR__ + '../templates/view-diagram-md.tpl', 'UTF-8'));
+var tplElement = function() {};
+var tplElementFila = function() {};
+var tplRelationship = _.template(readFully(__DIR__ + '../templates/relationship.tpl', 'UTF-8'));
+
+
 // Use Mustache.js-style templating
 _.templateSettings = {
     interpolate: /\{\{(.+?)\}\}/g

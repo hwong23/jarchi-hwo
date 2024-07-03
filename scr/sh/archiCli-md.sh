d3c303152301af98ddbfd30d026477fe4216e329
hwo <hwong23@gmail.com>
2024-07-03 15:00:29 -0500

    flujo1.archi-sh-js

--- scr/sh/archiCli-md.sh
+++ scr/sh/archiCli-md.sh
@@ -14,7 +14,11 @@ echo '   rutaprg' $config_development_rutaprg
 echo '   vistadoc' $config_development_vistadoc
 echo '   rutaexpportprg': $config_development_rutaexpportprg
 
-/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $config_development_rutamodelo --script.runScript $config_development_rutaprg -vistaDocumental $config_development_vistadoc
+/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.\
+commandline.app -consoleLog -nosplash --modelrepository.loadModel \
+$config_development_rutamodelo --script.runScript $config_development_rutaprg \
+-vistaDocumental $config_development_vistadoc
+
 
 # status=$?
 # echo 
--- scr/sh/archiCli-savemodel.sh
+++ scr/sh/archiCli-savemodel.sh
@@ -5,5 +5,7 @@
 # $3: ruta programa ajs
 
 
-/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $1/$2 --script.runScript $1/jarchi-hwo/examples/$3 -vistaDocumental $4
+/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app \
+-consoleLog -nosplash --modelrepository.loadModel $1/$2 \
+--script.runScript $1/jarchi-hwo/examples/$3 -vistaDocumental $4
 

f1c1bd36bddbb1a990e20497485defbebe761ade
harry.wong <hwong23@gmail.com>
2024-10-18 22:45:14 -0500

    drivingview--avance

--- scr/lib/devdocslib.js
+++ scr/lib/devdocslib.js
@@ -333,7 +333,7 @@ function devdoc_obtNota (theView) {
     var c = $(theView).find().not("element").not("relationship").first();
 
     // try {
-        if (c) {
+        if (c.text) {
             if ($(c).rels().length==0) {
                 notatexto = notatexto + c.text + '\n\n';
             }

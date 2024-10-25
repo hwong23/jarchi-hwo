a1d46102166d708cdf509a88f475b6c39fbeefbe
hwo <hwong23@gmail.com>
2024-10-25 17:03:09 -0500

    archiCLI-html

--- scr/sh/archiCli-html.sh
+++ scr/sh/archiCli-html.sh
@@ -1,22 +1,75 @@
-# Generación de documento MD basado en la vista documental marcada con el alias propertie del repositorio de arquitectura.
+# Generación de documento MD basado en la vista documental marcada con 
+# el alias propertie del repositorio de contenidos de arquitectura.
+
 
 # $1: ruta archivo zconfig.yml
+# $2: transformador Tx-MD
+# $3: vista fuente, modelo de contenidos 
+# $4: entrada de configuración
 
+
+# Validacion número de argumentos
+ if [ $# -lt 4 ]; then
+    echo "Error: requiere 4 argumentos"
+    echo '$1': ruta archivo zconfig.yml
+    echo '$2': transformador Tx-MD
+    echo '$3': vista fuente del modelo de contenidos 
+    echo '$4': entrada de configuración
+
+    exit 1
+  fi
+
+# Validacion argumentos vacíos
+# if [ -z "$1" ||  -z "$2"]; then
+#    echo "Error: Argument is empty!"
+#    exit 1
+#  fi
+ 
 
 # include parse_yaml function
 . parse_yaml.sh
-eval $(parse_yaml $1/zconfig.yml "config_")
+eval $(parse_yaml $1/zconfig.yml config_)
+
+rutausr=$(varvalue config_ $4 _rutausr)
+rutamodelo=$(varvalue config_ $4 _rutamodelo)
+rutaprg=$(varvalue config_ $4 _rutaprg)
+prg=$([ -z "$2"  ] && echo $(varvalue config_ $4 _expportprg) || echo $2)
+vistadoc=$3
+rutaMacMD=$(varvalue config_ $4 _rutaMacMD)
+rutaCompleta=$([ -z "$5"  ] && echo $(varvalue config_ $4 _rutaCompleta) || echo $5)
+
 echo Configuracion: 
-echo '   rutausr:' $config_development_rutausr
-echo '   rutamodelo:' $config_development_rutamodelo
-echo '   rutaprg' $config_development_rutaprg
-echo '   vistadoc' $config_development_vistadoc
-echo '   rutaexpportprg': $config_development_rutaexpportprg
+echo '   rutausr:' $rutausr
+echo '   rutamodelo:' $rutamodelo
+echo '   rutaprg' $rutaprg
+echo '   prg:' $prg
+echo '   vistadoc' $vistadoc
+echo '   rutaMacMD': $rutaMacMD
+echo '   rutaCompleta': $rutaCompleta
 
-# /Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $config_development_rutamodelo --script.runScript $config_development_rutaprg -vistaDocumental $config_development_vistadoc
 
-/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app \
--consoleLog -nosplash --modelrepository.loadModel $config_development_rutamodelo \
---script.runScript $config_development_rutaexpportprg -vistaDocumental \
-$config_development_vistadoc || echo "ERR"
+# /Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash \
+#    --modelrepository.loadModel $rutamodelo \
+#    --script.runScript $rutaprg/$prg \
+#    -vistaDocumental $vistadoc \
+#    -rutaMacMD $rutaMacMD \
+#    -rutaCompleta $rutaCompleta
+
+
+status=$?
+echo 
+echo exportSingle-htmlCLI
+echo 'prgexporthtml': $config_deploy_prgexporthtml
+echo 'rutaprgexporthtml': $config_deploy_rutaprgexporthtml
+
+prghtml=$([ -z "$5"  ] && echo $config_deploy_prgexporthtml || echo $2)
+rutaprgexporthtml=$config_deploy_rutaprgexporthtml
+
+
+[ $status -eq 0 ] && /Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app \
+-consoleLog -nosplash \
+   --modelrepository.loadModel $rutamodelo \
+   --script.runScript $rutaprg/$prghtml \
+   -rutaMacMD $rutaprgexporthtml \
+  || echo "ERR"
 

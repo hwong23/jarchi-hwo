38bd20c8e08ab83315cb7426cb8870afc5a13030
harry.wong <hwong23@gmail.com>
2024-12-28 21:14:48 -0500

    myargs

--- scr/lib/myargs.js
+++ scr/lib/myargs.js
@@ -1,9 +1,19 @@
 // HWO
 var argv = Java.type("org.eclipse.core.runtime.Platform").getApplicationArgs()
 
-function getArgumento (miArg) {
+function myargs_getArgumento (miArg) {
     // índice myArg
     var indice = argv.indexOf('-'+miArg);
     if (indice != -1) { return argv[indice+1] };
 }
+
+
+// Argumentos de entrada por CLI
+var argRutaMacMD = getArgumento("rutaMacMD");
+var argVistaDocumental = getArgumento("vistaDocumental");
+var argRutaCompleta = getArgumento("rutaCompleta");
+console.log("Entradas: ")
+debug? console.log("   theRutaMacMD: "+argRutaMacMD):true;
+debug? console.log("   vistaDocumental: "+argVistaDocumental):true;
+debug? console.log("   rutaCompleta: "+argRutaCompleta):true;
 

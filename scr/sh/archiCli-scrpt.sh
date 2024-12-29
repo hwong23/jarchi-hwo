51842196dfd2b3f491e5c4ac30339f99dec539e6
harry.wong <hwong23@gmail.com>
2024-12-29 18:26:39 -0500

    importcsv.3

--- scr/sh/archiCli-scrpt.sh
+++ scr/sh/archiCli-scrpt.sh
@@ -1,68 +1,45 @@
 # Generación de documento MD basado en la vista documental marcada con 
 # un alias (propertie) del repositorio de contenidos de arquitectura.
 
-# $1: rutausr
 # $2: rutamodelo
 # $3: rutaprg
-# $4: prg
-# $5: vistadoc
-# $6: rutasalida
+# $6: rutalectura
 
 # Ejemplo:
-# $1: $HOME/git
-# $2: $HOME/git/archigit/modelocontenidos
-# $3: $HOME/git/jarchi-hwo/scr
-# $4: CSV.importFromCLI.ajs
-# $5: cutemplate
-# $6: [$HOME] /Downloads/tmpr
+# $1: $HOME/git/archigit/modelocontenidos
+# $2: $HOME/git/jarchi-hwo/scr/CSV.importFromCLI.ajs
+# $3: [$HOME] /Downloads/tmpr/contenido.csv
 
 
-rutausr=$1
-rutamodelo=$2
-rutaprg=$3
-prg=$4
-vistadoc=$5
-rutasalida=$6
+rutamodelo=$1
+rutaprg=$2
+rutalectura=$3
 
 
 # Validacion número de argumentos
- if [ $# -lt 6 ]; then
+ if [ $# -lt 3 ]; then
     echo "Error: requiere 4 argumentos"
-    echo '$1': rutausr
-    echo '$2': rutamodelo
-    echo '$3': rutaprg
-    echo '$4': prg
-    echo '$5': vistadoc
-    echo '$6': rutasalida
+    echo '$1': rutamodelo
+    echo '$2': rutaprg
+    echo '$3': rutalectura
 
     exit 1
   fi
 
 # Validacion argumentos vacíos
-# if [ -z "$1" ||  -z "$2"]; then
-#    echo "Error: Argument is empty!"
-#    exit 1
-#  fi
- 
-
-# Validacion argumentos vacíos
-if [ -z $prg ]; then
-   echo "Error: El programa $tx no existe"
+if [ -z $rutalectura ]; then
+   echo "Error: ruta lectura '$rutalectura' no existe"
    exit 1
  fi
 
 
 echo Configuracion: 
-echo '   rutausr:     ' $rutausr
 echo '   rutamodelo:  ' $rutamodelo
 echo '   rutaprg:     ' $rutaprg
-echo '   prg:         ' $prg
-echo '   vistadoc:    ' $vistadoc
-echo '   rutasalida:  ' $rutasalida
+echo '   rutalectura: ' $rutalectura
 
 
 /Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash \
    --modelrepository.loadModel $rutamodelo \
-   --script.runScript $rutaprg/$prg \
-   -vistaDocumental $vistadoc \
-   -rutaSalida $rutasalida
+   --script.runScript $rutaprg \
+   -rutaLectura $rutalectura

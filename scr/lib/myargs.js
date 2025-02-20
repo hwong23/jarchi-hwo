05c1f0aa32129a3b292002bde43898c8a094ca67
harry.wong <hwong23@gmail.com>
2025-02-19 21:21:31 -0500

    Revert "indexof"

--- scr/lib/myargs.js
+++ scr/lib/myargs.js
@@ -3,7 +3,7 @@ var argv = Java.type("org.eclipse.core.runtime.Platform").getApplicationArgs()
 
 function myargs_getArgumento (miArg) {
     // índice myArg
-    var indice = argv.search('-'+miArg);
+    var indice = argv.indexOf('-'+miArg);
     if (indice != -1) { return argv[indice+1] };
 }
 

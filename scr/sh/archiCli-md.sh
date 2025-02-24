a33f25a45146f6e7651e5c1043f134cb36d482a3
hwo <hwong23@gmail.com>
2025-02-24 12:11:08 -0500

    simplif--zconfig

--- scr/sh/archiCli-md.sh
+++ scr/sh/archiCli-md.sh
@@ -62,10 +62,10 @@ echo "Purga contenidos $HOME$rutaMacMD"
 rm -Rf $HOME$rutaMacMD
 
 # archi -> /Applications/Archi.app/Contents/MacOS/Archi
-# /opt/Archi/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash \
-#    --modelrepository.loadModel $rutamodelo \
-#    --script.runScript $rutaprg/$prg \
-#    -vistaDocumental $vistadoc \
-#    -rutaMacMD $rutaMacMD \
-#    -rutaCompleta $rutaCompleta
+/opt/Archi/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash \
+   --modelrepository.loadModel $rutamodelo \
+   --script.runScript $rutaprg/$prg \
+   -vistaDocumental $vistadoc \
+   -rutaMacMD $rutaMacMD \
+   -rutaCompleta $rutaCompleta
 

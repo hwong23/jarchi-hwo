1f693ef713b95a7de86818270856b8100b8a303c
hwo <hwong23@gmail.com>
2025-02-24 12:10:41 -0500

    simplif--zconfig

--- scr/sh/archiCli-md.sh
+++ scr/sh/archiCli-md.sh
@@ -6,17 +6,16 @@
 
 # $1: ruta archivo zconfig.yml
 # $2: transformador Tx-MD
-# $3: vista fuente, modelo de contenidos 
-# $4: entrada de configuración
+# $3: entrada de configuración
 
+entradaextraccion=mdextraer
 
 # Validacion número de argumentos
- if [ $# -lt 4 ]; then
+ if [ $# -lt 3 ]; then
     echo "Error: requiere 4 argumentos"
     echo '$1': ruta archivo zconfig.yml
     echo '$2': transformador Tx-MD
-    echo '$3': vista fuente del modelo de contenidos 
-    echo '$4': entrada de configuración
+    echo '$3': entrada de configuración
 
     exit 1
   fi
@@ -32,14 +31,14 @@
 . ./parse_yaml.sh
 eval $(parse_yaml $1/zconfig.yml config_)
 
-rutausr=$(varvalue config_ $4 _rutausr)
-rutamodelo=$(varvalue config_ $4 _rutamodelo)
-rutaprg=$(varvalue config_ $4 _rutaprg)
-prg=$(echo $(varvalue config_ $4 _expportprg_$2))
-# prg=$([ $2 == "-" ] && echo $(varvalue config_ $4 _expportprg) || echo $2)
-vistadoc=$3
-rutaMacMD=$(varvalue config_ $4 _rutaMacMD)
-rutaCompleta=$([ -z "$5"  ] && echo $(varvalue config_ $4 _rutaCompleta) || echo $5)
+rutamodelo=$(varvalue config_ $entradaextraccion _rutamodelo)
+rutaprg=$(varvalue config_ $entradaextraccion _rutaprg)
+prg=$(echo $(varvalue config_ $3 _expportprg_$2))
+# prg=$([ $2 == "-" ] && echo $(varvalue config_ $entradaextraccion _expportprg) || echo $2)
+# vistadoc=$3
+vistadoc=$(varvalue config_ $entradaextraccion _devdocalias)
+rutaMacMD=$(varvalue config_ $entradaextraccion _rutaMacMD)
+rutaCompleta=$([ -z "$5"  ] && echo $(varvalue config_ $entradaextraccion _rutaCompleta) || echo $5)
 
 # Validacion argumentos vacíos
 if [ -z $prg ]; then
@@ -50,7 +49,6 @@ if [ -z $prg ]; then
 
 
 echo Configuracion: 
-echo '   rutausr:     ' $rutausr
 echo '   rutamodelo:  ' $rutamodelo
 echo '   rutaprg:     ' $rutaprg
 echo '   prg:         ' $prg
@@ -64,10 +62,10 @@ echo "Purga contenidos $HOME$rutaMacMD"
 rm -Rf $HOME$rutaMacMD
 
 # archi -> /Applications/Archi.app/Contents/MacOS/Archi
-/opt/Archi/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash \
-   --modelrepository.loadModel $rutamodelo \
-   --script.runScript $rutaprg/$prg \
-   -vistaDocumental $vistadoc \
-   -rutaMacMD $rutaMacMD \
-   -rutaCompleta $rutaCompleta
+# /opt/Archi/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash \
+#    --modelrepository.loadModel $rutamodelo \
+#    --script.runScript $rutaprg/$prg \
+#    -vistaDocumental $vistadoc \
+#    -rutaMacMD $rutaMacMD \
+#    -rutaCompleta $rutaCompleta
 

2a6d9b055a7c96017cee5a21fc9690037c05805d
hwo <hwong23@gmail.com>
2025-03-11 16:14:23 -0500

    cpushcontd

--- scr/sh/cpushcontd.sh
+++ scr/sh/cpushcontd.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 # Actualiza contenidos MD generado por Archi
+# $1: ruta archivo zconfig.yml
+# $2: msj commit
 
-
-# include parse_yaml function
-. ./parse_yaml.sh
-eval $(parse_yaml $1/zconfig.yml "config_")
-echo Configuracion: 
-echo '   gitorigen:' $config_publish_gitorigen
-echo '   comentario:' $2
+entradaextraccion=mdextraer
 
 # Validacion número de argumentos
  if [ $# -lt 3 ]; then
@@ -19,21 +15,24 @@ echo '   comentario:' $2
     exit 1
   fi
 
+# include parse_yaml function
+. ./parse_yaml.sh
+eval $(parse_yaml $1/zconfig.yml "config_")
 
-# $1: ruta archivo zconfig.yml
-# $2: msj commit
+rutaMacMD=$(varvalue config_ $entradaextraccion _rutaMacMD)
+echo '   rutaMacMD:' $rutaMacMD
 
-cd $config_publish_gitorigen
-cp webpage/index.html ..
+# cd $config_publish_gitorigen
+# cp webpage/index.html ..
  
  
-git checkout arq
-git pull --rebase
+# git checkout arq
+# git pull --rebase
 
-[ $? -eq 0 ] && cp -R ../index.html ./ || echo "ERR"
+# [ $? -eq 0 ] && cp -R ../index.html ./ || echo "ERR"
 
-git add .
-git commit -a -m "$2"
-git push
+# git add .
+# git commit -a -m "$2"
+# git push
  
-git checkout main
+# git checkout main

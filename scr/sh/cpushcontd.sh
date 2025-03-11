b2f4e0a5eb193877473ceeca02c6fccd0dd063d9
hwo <hwong23@gmail.com>
2025-03-11 16:39:11 -0500

    cpushcontd-git

--- scr/sh/cpushcontd.sh
+++ scr/sh/cpushcontd.sh
@@ -21,8 +21,11 @@ eval $(parse_yaml $1/zconfig.yml "config_")
 echo '   rutaMacMD:' $config_mdextraer_rutaMacMD
 echo '   rutaDeployContd:' $config_mdextraer_rutaDeployContd
 
-cd $HOME
-git clone https://github.com/hwong23/devocs-contd.git
+
+git clone https://github.com/hwong23/devocs-contd.git $HOME
+cd cd $HOME/devocs-contd
+git checkout -b ${{github.ref}} origin/${{github.ref}}
+
 
 ls -l $HOME/devocs-contd
 

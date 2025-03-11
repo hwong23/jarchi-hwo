ac7ab9ee789c175f44b0907165e449987e172f23
hwo <hwong23@gmail.com>
2025-03-11 16:24:22 -0500

    cpushcontd

--- scr/sh/cpushcontd.sh
+++ scr/sh/cpushcontd.sh
@@ -18,10 +18,9 @@ entradaextraccion=mdextraer
 . ./parse_yaml.sh
 eval $(parse_yaml $1/zconfig.yml "config_")
 
-rutaMacMD=$(varvalue config_ $entradaextraccion _rutaMacMD)
-rutaMacMD2=$config_mdextraer_rutaMacMD
-echo '   rutaMacMD:' $rutaMacMD
-echo '   rutaMacMD:' $rutaMacMD2
+echo '   rutaMacMD:' $config_mdextraer_rutaMacMD
+echo '   rutaDeployContd:' $config_mdextraer_rutaDeployContd
+
 
 # cd $config_publish_gitorigen
 # cp webpage/index.html ..

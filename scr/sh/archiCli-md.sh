2c8b56fe5261d31da6e786f230c4fcc7fb586505
hwo <hwong23@gmail.com>
2025-03-17 17:28:16 -0500

    purga

--- scr/sh/archiCli-md.sh
+++ scr/sh/archiCli-md.sh
@@ -73,10 +73,10 @@ fi
 #    -rutaMacMD $rutaMacMD \
 #    -rutaCompleta $rutaCompleta"
 
-# /opt/Archi/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash \
-#    --modelrepository.loadModel $rutamodelo \
-#    --script.runScript $rutaprg/$prg \
-#    -vistaDocumental $vistadoc \
-#    -rutaMacMD $RUTACONTD \
-#    -rutaCompleta $rutaCompleta
+/opt/Archi/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash \
+   --modelrepository.loadModel $rutamodelo \
+   --script.runScript $rutaprg/$prg \
+   -vistaDocumental $vistadoc \
+   -rutaMacMD $RUTACONTD \
+   -rutaCompleta $rutaCompleta
 

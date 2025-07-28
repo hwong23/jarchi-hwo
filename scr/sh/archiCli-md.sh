cf8daccb257fb522d041528bfacc2ec84bfa0335
hwong <hwong23@gmail.com>
2025-07-27 22:37:41 -0500

    purga

--- scr/sh/archiCli-md.sh
+++ scr/sh/archiCli-md.sh
@@ -57,7 +57,7 @@ echo '   rutaCompleta:' $rutaCompleta
 
 # Purga contenidos
 RUTACONTD=$(eval echo $rutaMacMD)
-ls $RUTACONTD
+ls -l $pwd
 rm -Rf $RUTACONTD
 if [ -z "$( ls -A $RUTACONTD )" ]; then
    echo "OK. Purga de contenidos exitosa $RUTACONTD"

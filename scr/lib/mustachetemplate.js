d7ca5014dd4914cdd36c3621ed73c6ecebd2c697
hwo <hwong23@gmail.com>
2025-08-05 13:57:49 -0500

    mustache

--- scr/lib/mustachetemplate.js
+++ scr/lib/mustachetemplate.js
@@ -1,6 +1,7 @@
 load(__DIR__ + 'nashorn-polyfills.js');
-load(__DIR__ + 'underscore-min.js');
-load(__DIR__ + 'marked.min.js');
+// load(__DIR__ + 'underscore-min.js');
+const _ = require(__DIR__ + "underscore-min.js");
+const marked = require(__DIR__ + 'marked.min.js');
 
 
 // Use Mustache.js-style templating

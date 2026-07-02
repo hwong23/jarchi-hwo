035e60c74890b59f9f19ab2c3d6c41c90be842d2
HWO <hwong23@gmail.com>
2026-07-01 20:32:14 -0500

    debug

--- scr/sh/archiCli-md.sh
+++ scr/sh/archiCli-md.sh
@@ -3,54 +3,44 @@
 # Generación de documento MD basado en la vista documental marcada con 
 # un alias (propertie) del repositorio de contenidos de arquitectura.
 
-
 # $1: ruta archivo zconfig.yml
 # $2: transformador Tx-MD [dd | solo]
 # $3: entrada de configuración
-# $4: alias de la vista documental (opcional). Si es "alias" o no se provee, se usa el valor desde zconfig.yml
-
+# $4: alias de la vista documental (opcional)
+# $5: debug (opcional, true|false). Default: false
+# $6: rutaCompleta (opcional)
 
 entradaextraccion=mdextraer
 
-# Validacion número de argumentos
- if [ $# -lt 3 ]; then
-    echo "Error: requiere 4 argumentos"
+if [ $# -lt 3 ]; then
+    echo "Error: requiere al menos 3 argumentos"
     echo '$1': ruta archivo zconfig.yml
     echo '$2': transformador Tx-MD
     echo '$3': entrada de configuración
-    echo '$4': alias de la vista documental *opcional*
-
+   #  echo '$4': alias de la vista documental *opcional*
+   #  echo '$5': debug *opcional* (true|false)
+   #  echo '$6': rutaCompleta *opcional*
     exit 1
-  fi
+fi
 
-# Validacion argumentos vacíos
-# if [ -z "$1" ||  -z "$2"]; then
-#    echo "Error: Argument is empty!"
-#    exit 1
-#  fi
- 
-
-# include parse_yaml function
 . ./parse_yaml.sh
 eval $(parse_yaml $1/zconfig.yml config_)
 
 rutamodelo=$(varvalue config_ $entradaextraccion _rutamodelo)
 rutaprg=$(varvalue config_ $entradaextraccion _rutaprg)
 prg=$(echo $(varvalue config_ $3 _expportprg_$2))
-# prg=$([ $2 == "-" ] && echo $(varvalue config_ $entradaextraccion _expportprg) || echo $2)
-# vistadoc=$(varvalue config_ $entradaextraccion _devdocalias)
-# Nuevo: aliasParam toma $4 o "alias" por defecto
-aliasParam=${4:-alias}
-vistadoc=$([ "$aliasParam" != "alias" ] && echo "$aliasParam" || echo $(varvalue config_ $entradaextraccion _devdocalias))
-rutaMacMD=$(varvalue config_ $entradaextraccion _rutaMacMD)/$3
-rutaCompleta=$([ -z "$5"  ] && echo $(varvalue config_ $entradaextraccion _rutaCompleta) || echo $5)
 
-# Validacion argumentos vacíos
-if [ -z $prg ]; then
+aliasParam=${4:-alias}
+debugParam=${5:-false}
+vistadoc=$([ "$aliasParam" != "alias" ] && echo "$aliasParam" || echo $(varvalue config_ $entradaextraccion _devdocalias))
+
+rutaMacMD=$(varvalue config_ $entradaextraccion _rutaMacMD)/$3
+rutaCompleta=$([ -z "$6" ] && echo $(varvalue config_ $entradaextraccion _rutaCompleta) || echo $6)
+
+if [ -z "$prg" ]; then
    echo "Error: El transformadorTx $tx no existe."
    exit 1
- fi
-
+fi
 
 echo Configuracion: 
 echo '   rutamodelo:  ' $rutamodelo
@@ -59,9 +49,8 @@ echo '   prg:         ' $prg
 echo '   vistadoc:    ' $vistadoc
 echo '   rutaMacMD:   ' $rutaMacMD
 echo '   rutaCompleta:' $rutaCompleta
+echo '   debug:       ' $debugParam
 
-
-# Purga contenidos
 RUTACONTD=$(eval echo $rutaMacMD)
 rm -Rf $RUTACONTD
 if [ -z "$( ls -A $RUTACONTD )" ]; then
@@ -70,19 +59,14 @@ else
    echo "WARN. Purga fallida $RUTACONTD: carpeta de trabajo tiene contenidos"
 fi
 
-# archi -> /Applications/Archi.app/Contents/MacOS/Archi
-# echo "Archi -application com.archimatetool.commandline.app -consoleLog -nosplash \
-#    --modelrepository.loadModel $rutamodelo \
-#    --script.runScript $rutaprg/$prg \
-#    -vistaDocumental $vistadoc \
-#    -rutaMacMD $rutaMacMD \
-#    -rutaCompleta $rutaCompleta"
-
 /opt/Archi/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash \
    --modelrepository.loadModel $rutamodelo \
    --script.runScript $rutaprg/$prg \
    -vistaDocumental $vistadoc \
    -rutaMacMD $RUTACONTD \
-   -rutaCompleta $rutaCompleta
+   -rutaCompleta $rutaCompleta \
+   -depuracion $debugParam
+
+
 
 

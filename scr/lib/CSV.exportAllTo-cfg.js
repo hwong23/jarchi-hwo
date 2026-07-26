0a797d537f3ad7ad07e17a17cdec55336cccc26c
hwo <hwong23@gmail.com>
2026-07-26 16:34:43 -0500

    CSV.exportcfg

---
+++ scr/lib/CSV.exportAllTo-cfg.js
@@ -0,0 +1,12 @@
+// define la configuración de la exportación
+const cfg_mdOptions = {
+    gfm: true,
+    breaks: true,
+    smartLists: true,
+    smartypants: true,
+    debug : false,
+    userPath : "/Users/hwo/Documents/work/INGEN/modelexport",
+    inclyePropiedades : false,
+    repeticion : true,
+    noEnter : true
+};

f78c64dd62058c608dcc17ef17d0b7a3a44e1053
hwo <hwong23@gmail.com>
2026-08-29 14:30:46 -0500

    matrixcfg

---
+++ scr/lib/mb-matrixfrommodel.js
@@ -0,0 +1,13 @@
+// define la configuración de la exportación
+const cfg_mdOptions = {
+    gfm: true,
+    breaks: true,
+    smartLists: true,
+    smartypants: true,
+    debug : false,
+    userPath : "/Users/hwo/Downloads",
+    columnType: "business-process",
+    rowType: "application-component",
+    tabName: "app-procesos",
+    cellSeparator: "/"
+};
